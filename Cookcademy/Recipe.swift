d1ed8f383bc534a1889a8a717734f9ca3ef22c43
Elvis <elvismendez1998@gmail.com>
2023-03-09 17:45:00 -0600

    Add enums for Category and unit,  new struct for direction

--- Cookcademy/Recipe.swift
+++ Cookcademy/Recipe.swift
@@ -9,15 +9,40 @@ import Foundation
 
 struct Recipe {
     var mainInformation: MainInformation
-    var ingredients: [String]
-    var directions: [String]
+    var ingredients: [Ingredient]
+    var directions: [Direction]
 }
 
 struct MainInformation {
     var name: String
     var description: String
     var author: String
-    var category: String // Breakfast, Lunch, Dinner
+    var category: Category // Breakfast, Lunch, Dinner
     
+    enum  Category: String, CaseIterable {
+        case breakfast = "Breakfast"
+        case lunch = "Lunch"
+        case dinner = "Dinner"
+        case dessert = "Dessert"
+    }
+}
+
+struct Ingredient {
+    var name: String
+    var quantity: Double
+    var unit: Unit // Oz, Grams, Tablespoons, Teaspoons, none
     
+    enum Unit: String, CaseIterable {
+        case oz = "Ounces"
+        case g = "Grams"
+        case cups = "Cups"
+        case tbs = "Tablespoons"
+        case tsb = "Teaspoons"
+        case none = "No units"
+    }
+}
+
+struct Direction {
+    var description: String
+    var isOptional: Bool
 }
