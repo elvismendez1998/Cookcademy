0f512c5a173462b8484bee92c4e07d1a3f599db0
Elvis <elvismendez1998@gmail.com>
2023-03-09 17:12:48 -0600

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Elvis Mendez on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
