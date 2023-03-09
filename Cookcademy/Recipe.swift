f3763f1266a2b88dbb05d6297101f74305531eba
Elvis <elvismendez1998@gmail.com>
2023-03-09 17:21:11 -0600

    Add starting recipe model

---
+++ Cookcademy/Recipe.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe.swift
+//  Cookcademy
+//
+//  Created by Elvis Mendez on 3/9/23.
+//
+
+import Foundation
+
+struct Recipe {
+    var name: String
+    var description: String
+    var author: String
+    var category: String // Breakfast, Lunch, Dinner
+    var ingredients: [String]
+    var directions: [String]
+}
